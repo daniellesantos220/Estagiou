701343316f9e12a0d658efc68f337246b8349bb7
Danielle Carvalho <daniellesantossilva220@gmail.com>
2025-10-15 21:50:01 -0300

    relacionamento vaga

--- model/vaga_model.py
+++ model/vaga_model.py
@@ -1,5 +1,10 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
+
+from model.area_model import Area
+from model.empresa_model import Empresa
+from model.usuario_model import Usuario
 
 
 @dataclass
@@ -13,3 +18,7 @@ class Vaga:
     numero_vagas: int
     salario: float
     data_cadastro: datetime
+
+    area: Optional[Area]
+    empresa: Optional[Empresa]
+    recrutador: Optional[Usuario]

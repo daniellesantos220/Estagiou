27db0f0375a40f62ea6b839f6b242791b77222b7
Danielle Carvalho <daniellesantossilva220@gmail.com>
2025-11-04 21:52:53 -0300

    rotas e templates adicionados

--- model/curtida_model.py
+++ model/curtida_model.py
@@ -1,11 +1,11 @@
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 @dataclass
 
 class Curtida:
-    id: int
-    usuario_id: int
+    id_usuario: int
     id_vaga: int
-    data_curtida: datetime
+    data_curtida: Optional[datetime] = None
     
